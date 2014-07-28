edc0ebf7e6aa4e16cd27519e04304f4f45ea3b87
luna <lunaceee@gmail.com>
2014-07-27 18:17:59 -0700

    converted the speech website into a form fill

--- chinese_speech_tool/speech.js
+++ chinese_speech_tool/speech.js
@@ -5,6 +5,23 @@ var recognizing = false;
 var ignore_onend = false;
 var start_timestamp;
 
+
+function showStandard(){
+  standard_button.style.visibility = 'visible';
+}
+
+
+$("textarea").on('paste keyup',showStandard);
+
+
+function standardAccent(){
+  val_of_textarea = $('#userinput').val();
+  var url = 'http://api.ispeech.org/api/rest?apikey=7955b90cb40f63db22de72cd2822df49%20&action=convert&text='+ val_of_textarea +'&voice=chchinesefemale&format=mp3&speed=1';
+  var myWindow = window.open(url,"width=200,height=100");
+}
+
+$("#standard_button").on('click',standardAccent);
+
 function upgrade() {
   start_button.style.visibility = 'hidden';
   showInfo('info_upgrade');
@@ -33,14 +50,14 @@ if (!('webkitSpeechRecognition' in window)) {
   recognition.interimResults = true;
 
   recognition.onstart = function() {
-  	console.log('on_start');
+    console.log('on_start');
     recognizing = true;
     showInfo('info_speak_now');
     start_img.src = 'img/mic-animate.gif';
   };
 
   recognition.onerror = function(event) {
-  	  	console.log('on_error');
+        console.log('on_error');
     if (event.error == 'no-speech') {
       start_img.src = 'img/mic.gif';
       showInfo('info_no_speech');
@@ -62,7 +79,7 @@ if (!('webkitSpeechRecognition' in window)) {
   };
 
   recognition.onend = function() {
-  	  	console.log('on_end');
+        console.log('on_end');
     recognizing = false;
     if (ignore_onend) {
       return;
@@ -83,7 +100,7 @@ if (!('webkitSpeechRecognition' in window)) {
       window.getSelection().addRange(range);
     }
 
-    var desiredPhrase = phraseArray[current_phrase];
+    var desiredPhrase = $('#userinput').val();
     var new_final_transcript = '';
     for (var stringIndex = 0; stringIndex < desiredPhrase.length; stringIndex += 1){
       console.log('the current letter is ' + desiredPhrase[stringIndex]);
@@ -112,10 +129,10 @@ if (!('webkitSpeechRecognition' in window)) {
   };
 
   recognition.onresult = function(event) {
-  	  	console.log('on_result');
+        console.log('on_result');
     var interim_transcript =  '';
     if (typeof(event.results) == 'undefined') {
-      console.log('got an undefined event');	
+      console.log('got an undefined event');  
       recognition.onend = null;
       recognition.stop();
       upgrade();
@@ -135,7 +152,7 @@ if (!('webkitSpeechRecognition' in window)) {
     final_span.innerHTML = linebreak(final_transcript);
     interim_span.innerHTML = linebreak(interim_transcript);
     if (final_transcript || interim_transcript) {
-      showButtons('inline-block');
+      showButtons('block');
     }
   };
 }
@@ -170,13 +187,12 @@ function showButtons(style) {
   }
   current_style = style;
   redo_button.style.display = style;
-  next_button.style.display = style;
-  standard_button.style.display = style;
+  // standard_button.style.display = style;
 
-  console.log('back');
-  if (first_phrase == false){
-    back_button.style.display = style;
-  }
+  // // console.log('back');
+  // // if (first_phrase == false){
+  // //   back_button.style.display = style;
+  // }
 }
 
 function clearResult(){
@@ -190,7 +206,6 @@ function newPhrase(){
     return;
   }
   first_phrase = false;
-  back_button.style.display = 'inline';
   current_phrase = current_phrase + 1;
   phrase.innerHTML = phraseArray[current_phrase];
   pinyin.innerHTML = pinyinArray[current_phrase];
@@ -207,22 +222,21 @@ function previousPhrase(){
   pinyin.innerHTML = pinyinArray[current_phrase];
 }
 
-var current_standard = 0;
-function standardAccent(){
- console.log(standardArray[current_phrase]);
- console.log(current_phrase);
- var myWindow = window.open(standardArray[current_phrase],"width=200,height=100");
+// var current_standard = 0;
+
  //standard_button.innerHTML = standardArray[]
-}
 
-var phraseArray = ['你会说英文吗','我爱啤酒','算你狠','隔行如隔山','情人眼里出西施'];
-var pinyinArray = ['nǐ huì shuō yīng wén ma','wǒ ài píjiǔ','suàn nǐ hěn','gé háng rú gé shān','qíng rén yǎn lǐ chū xī shī'];
-var standardArray = ["http://api.ispeech.org/api/rest?apikey=7955b90cb40f63db22de72cd2822df49%20&action=convert&text=%E4%BD%A0%E4%BC%9A%E8%AF%B4%E8%8B%B1%E6%96%87%E5%90%97&voice=chchinesefemale&format=mp3&speed=1",
-"http://api.ispeech.org/api/rest?apikey=7955b90cb40f63db22de72cd2822df49%20&action=convert&text=%E6%88%91%E7%88%B1%E5%95%A4%E9%85%92&voice=chchinesefemale&format=mp3&speed=1",
-"http://api.ispeech.org/api/rest?apikey=7955b90cb40f63db22de72cd2822df49%20&action=convert&text=%E7%AE%97%E4%BD%A0%E7%8B%A0&voice=chchinesefemale&format=mp3&speed=1",
-"http://api.ispeech.org/api/rest?apikey=7955b90cb40f63db22de72cd2822df49%20&action=convert&text=%E9%9A%94%E8%A1%8C%E5%A6%82%E9%9A%94%E5%B1%B1&voice=chchinesefemale&format=mp3&speed=1",
-"http://api.ispeech.org/api/rest?apikey=7955b90cb40f63db22de72cd2822df49%20&action=convert&text=%E6%83%85%E4%BA%BA%E7%9C%BC%E9%87%8C%E5%87%BA%E8%A5%BF%E6%96%BD&voice=chchinesefemale&format=mp3&speed=1"];
+// var phraseArray = ['你会说英文吗','我爱啤酒','算你狠','隔行如隔山','情人眼里出西施'];
+// var pinyinArray = ['nǐ huì shuō yīng wén ma','wǒ ài píjiǔ','suàn nǐ hěn','gé háng rú gé shān','qíng rén yǎn lǐ chū xī shī'];
+// var standardArray = ["http://api.ispeech.org/api/rest?apikey=7955b90cb40f63db22de72cd2822df49%20&action=convert&text=%E4%BD%A0%E4%BC%9A%E8%AF%B4%E8%8B%B1%E6%96%87%E5%90%97&voice=chchinesefemale&format=mp3&speed=1",
+// "http://api.ispeech.org/api/rest?apikey=7955b90cb40f63db22de72cd2822df49%20&action=convert&text=%E6%88%91%E7%88%B1%E5%95%A4%E9%85%92&voice=chchinesefemale&format=mp3&speed=1",
+// "http://api.ispeech.org/api/rest?apikey=7955b90cb40f63db22de72cd2822df49%20&action=convert&text=%E7%AE%97%E4%BD%A0%E7%8B%A0&voice=chchinesefemale&format=mp3&speed=1",
+// "http://api.ispeech.org/api/rest?apikey=7955b90cb40f63db22de72cd2822df49%20&action=convert&text=%E9%9A%94%E8%A1%8C%E5%A6%82%E9%9A%94%E5%B1%B1&voice=chchinesefemale&format=mp3&speed=1",
+// "http://api.ispeech.org/api/rest?apikey=7955b90cb40f63db22de72cd2822df49%20&action=convert&text=%E6%83%85%E4%BA%BA%E7%9C%BC%E9%87%8C%E5%87%BA%E8%A5%BF%E6%96%BD&voice=chchinesefemale&format=mp3&speed=1"];
 
 
-phrase.innerHTML = phraseArray[0];
-pinyin.innerHTML = pinyinArray[0];
+
+
+// phrase.innerHTML = phraseArray[0];
+// pinyin.innerHTML = pinyinArray[0];
+
