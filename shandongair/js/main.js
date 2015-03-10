33a323f7d9af152e4a40f595f3c7b333b263be1e
luna <lunaceee@gmail.com>
2015-03-10 12:48:03 +0700

    changed file name

---
+++ shandongair/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+		$(".fancybox").fancybox();
+		
+		$(".various").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+
+
+			 $('.viewport').mouseenter(function(e) {
+				// $(this).children('a').children('img').animate({ left: '0', top: '0', width: '100%'}, 100);
+				$(this).children('a').children('span').fadeIn(200).css("display","block");
+					}).mouseleave(function(e) {
+				$(this).children('a').children('img').animate({ left: '-10%', top: '-10%', width: '110%'}, 100);
+				$(this).children('a').children('span').fadeOut(200);
+			});
+
+});
