44ff7b352466d40736010998e10e5e27473bfc6d
luna <lunaceee@gmail.com>
2015-03-18 22:51:21 +0700

    added and removed new files

--- shandongair/js/main.js
+++ shandongair/js/main.js
@@ -15,12 +15,18 @@ $(document).ready(function() {
 	});
 
 
-			 $('.viewport').mouseenter(function(e) {
+		// home page video fancybox
+	$('.viewport').mouseenter(function(e) {
 				// $(this).children('a').children('img').animate({ left: '0', top: '0', width: '100%'}, 100);
 				$(this).children('a').children('span').fadeIn(200).css("display","block");
 					}).mouseleave(function(e) {
-				$(this).children('a').children('img').animate({ left: '-10%', top: '-10%', width: '110%'}, 100);
+				// $(this).children('a').children('img').animate({ left: '-10%', top: '-10%', width: '110%'}, 100);
 				$(this).children('a').children('span').fadeOut(200);
 			});
+
+
+
+		// course page tabs
+
 
 });
