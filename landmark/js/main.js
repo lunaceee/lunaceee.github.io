4b6be7ba30a198e1bb69dade4b2e3aaf336ef66d
luna <lunaceee@gmail.com>
2017-01-24 23:16:45 -0800

    auto-publish commit by publish.py script

--- landmark/js/main.js
+++ landmark/js/main.js
@@ -32,7 +32,7 @@ var initialScene = new ScrollMagic.Scene({
     offset: 1,
 })
 .on("leave", function () {
-    $("#preloader-day").css("background-image","url(../css/images/day-theatre.svg)");
+    $("#preloader-day").css("background-image","url(css/images/day-theatre.svg)");
     $(".loading-bg").css("background-color","#ebe2d0");
     $("nav ul a").css("color","#795548");
 
@@ -47,7 +47,7 @@ var sceneLoading = new ScrollMagic.Scene({
 .on("enter", function () {
 		// trigger animation by changing inline style.
 		//preloaderDay.style.backgroundImage = "/css/images/preloader-night.svg";
-	$("#preloader-day").css("background-image","url(../css/images/night-theatre.svg)");
+	$("#preloader-day").css("background-image","url(css/images/night-theatre.svg)");
     $(".loading-bg").css({"background-color":"#535352","transition":"all 2s ease-in-out"});
     $("nav ul a").css("color","#BDBDBD");
 
